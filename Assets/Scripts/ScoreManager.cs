4b98e800086f6c1badcb121e2b93be92ec6d9d3e
Jarideldaele <jari.deldaele10@gmail.com>
2024-04-30 11:52:10 +0200

    pointSystem
    Made the UI/text for the pointSystem

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    public Text scoreP1Text;
+    public Text scoreP2Text;
+
+    int scoreP1 = 0;
+    int scoreP2 = 0;
+
+   
+    void Start()
+    {
+        scoreP1Text.text = "Player1 Score: " + scoreP1.ToString();
+        scoreP2Text.text = "Player2 Score: " + scoreP2.ToString();
+
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
